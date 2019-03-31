5b7b72d089115b06c5b971e7ce9b8fdf4356a981
jagatguru31 <jagatguru31@gmail.com>
2019-03-31 19:52:59 +0530

    added angular

--- djangular/djangular/scrumboard/urls.py
+++ djangular/djangular/scrumboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .api import ListApi,CardApi
+from django.views.generic import TemplateView
 
 urlpatterns =[
     path('lists',ListApi.as_view()),
     path('cards',CardApi.as_view()),
+    path('home',TemplateView.as_view(template_name="index.html"))
 ]
